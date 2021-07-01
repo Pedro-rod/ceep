2895d26ff85a4627bdff01d5c3ca83bc3634a51b
Pedro Henrique Rodrigues <pedrohrprimo@gmail.com>
2021-06-30 21:57:59 -0300

    Project base

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+import ListaDeNotas from "./components/ListaNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias"
+import Categorias from "./dados/categorias"; 
+import ArrayDeNotas from "./dados/arrayDeNotas";
+
+import "./assets/App.css";
+import "./assets/index.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro 
+          categorias={this.categorias}
+          criarNota={this.notas.adicionarNota.bind(this.notas)} 
+        />
+        <main className="conteudo-principal">
+          <ListaDeCategorias 
+            categorias={this.categorias} 
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+          />
+          <ListaDeNotas
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+            notas={this.notas}
+          />
+        </main>
+      </section>
+    );
+  }
+}
+
+export default App;
